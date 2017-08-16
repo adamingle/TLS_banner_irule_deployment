62ab423a12c41a467ee8addf20e5791f2de0d0d8
adamingle <31011568+adamingle@users.noreply.github.com>
2017-08-16 16:28:42 -0500

    Create goforward_script.tcl

---
+++ goforward_script.tcl
@@ -0,0 +1,29 @@
+proc script::run {} {
+if { $tmsh::argc != 2 } {
+puts "A single rule name must be provided"
+exit
+}
+set rulename [lindex $tmsh::argv 1]
+set rules ""
+set vips [tmsh::get_config /ltm virtual]
+set vips_in_play ""
+tmsh::begin_transaction
+foreach vip $vips {
+if { [tmsh::get_name $vip] contains "BGP" }{
+if { [tmsh::get_field_value $vip "rules" rules] != 0 } {
+set addrule [concat $rules $rulename ]
+tmsh::modify /ltm virtual [tmsh::get_name $vip] rules "{ $addrule }" profiles add "{" /Common/stream { } jha_httpcompression { } "}"
+lappend vips_in_play $vip
+}
+else {
+tmsh::modify /ltm virtual [tmsh::get_name $vip] rules { WS_TLS_banner_irule } profiles add "{" /Common/stream { } jha_httpcompression { } "}"
+lappend vips_in_play $vip
+}
+}
+}
+tmsh::commit_transaction
+puts "The $rulename iRule was added to the following virtuals: "
+foreach x $vips_in_play {
+puts "\t[tmsh::get_name $x]"
+}
+}
